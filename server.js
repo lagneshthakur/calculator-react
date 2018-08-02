b2addd38d1019629d058c7b5e5a3636b2ad505cc
Lagnesh Thakur <lagneshthakur@gmail.com>
2018-08-03 03:21:22 +0530

    added server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist'));
+
+// redirect all requests to index for front-end route handling
+app.get('/*', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+  });
+
+// Start the app by listening on the default port
+app.listen(process.env.PORT || 9000);
+
